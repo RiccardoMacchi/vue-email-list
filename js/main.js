6f3f4829332dca217e3ecc43be2197c32c0ad107
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-27 15:11:48 +0200

    aggiunto minimo stile css

--- js/main.js
+++ js/main.js
@@ -8,19 +8,19 @@ createApp({
   },
   methods: {
     getEmailApi(){
-
+        for (let i = 0; i < 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((risposta) => {
+                console.log(risposta.data.response);
+                this.emails.push(risposta.data.response);
+                console.log(this.emails)
+            });
+        }
     }
 
   },
   mounted(){
     console.log("montata")
-    for (let i = 0; i < 10; i++){
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((risposta) => {
-            console.log(risposta.data.response);
-            this.emails.push(risposta.data.response);
-            console.log(this.emails)
-        });
-    }
+    this.getEmailApi()
   }
 }).mount('#app')
