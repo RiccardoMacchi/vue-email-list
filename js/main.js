3b4437d44b0127ce2195e4d1394e0882d506a10f
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-27 14:59:00 +0200

    aggiunta ciclo per stampa 10 email aggiunta ciclo in html per stampa singola email da arrey emails

--- js/main.js
+++ js/main.js
@@ -14,11 +14,13 @@ createApp({
   },
   mounted(){
     console.log("montata")
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((risposta) => {
-        console.log(risposta.data.response);
-        this.emails.push(risposta.data.response);
-        console.log(this.emails)
-    });
+    for (let i = 0; i < 10; i++){
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((risposta) => {
+            console.log(risposta.data.response);
+            this.emails.push(risposta.data.response);
+            console.log(this.emails)
+        });
+    }
   }
 }).mount('#app')
