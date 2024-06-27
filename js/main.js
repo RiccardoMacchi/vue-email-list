3c627246d216ca7489e267d06368d1e51c80d218
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-27 14:54:00 +0200

    creazione logica aggiunta email da API in arrey in js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,22 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      emails : []
     }
+  },
+  methods: {
+    getEmailApi(){
+
+    }
+
+  },
+  mounted(){
+    console.log("montata")
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((risposta) => {
+        console.log(risposta.data.response);
+        this.emails.push(risposta.data.response);
+        console.log(this.emails)
+    });
   }
 }).mount('#app')
